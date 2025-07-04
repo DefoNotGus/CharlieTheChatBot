f49b7f8a99076a2d7b0a2c4b7628425baad410a9
DefoNotGus <grangel12345@gmail.com>
2025-07-04 18:37:15 +0100

    ASP.NET FE created and dockerized

---
+++ ChatbotTest/README.md
@@ -0,0 +1,2 @@
+# ChatbotTest
+A Frontend ASP.NET used to create a self-contained Chatbot
---
+++ ChatbotTest/Views/Shared/_Chatbot.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ChatbotTest.Views.Shared
+{
+    public class _ChatbotModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
